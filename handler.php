dc7669de1361a661a2dbf8d631ae9e99258d7849
Noah-Zwolak <144242513+Noah-Zwolak@users.noreply.github.com>
2023-11-03 13:16:35 +0000

    bad code

--- handler.php
+++ handler.php
@@ -17,13 +17,26 @@
     echo "<h1>My Program</h1>\n";
     echo "<p>My Variable is = ".$myVariable."</p>\n";
     ?>
-    if $myVariable=L ;
+    if $myVariable=large ;
        print(Your large pizza is $6)
-    elseif $myVariable=XL
+    elseif $myVariable= Extra large;
       print(Your extra large pizza is $10)
      
-     
-     
+      <?php
+    $Topping = "";
+    if ( isset( $_POST['Topping'] ) ){
+      $myVariable = $_POST['Topping'];
+    }
+    echo "<h1>My Program</h1>\n";
+    echo "<p>My Variable is = ".$Topping."</p>\n";
+    ?>
+if $Topping = 1;
+print(Your one topping is $1)
+elseif $Topping = 2 ;
+print(Your two toppings are $1.75 )
+elseif $Topping = 3;
+print(Your toppings cost $2.50)
+else print(Your for toppings are $3.35)
   </body>
   
 </html>
